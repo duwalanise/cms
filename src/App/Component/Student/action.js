402c4afca77c8b34b00e920525f7ad30ee8ffde7
Anish Duwal <duwalanise@gmail.com>
2017-11-06 12:22:10 +0545

    update api url

--- src/App/Component/Student/action.js
+++ src/App/Component/Student/action.js
@@ -1,60 +1,58 @@
-export const fetchStudentList = () =>
-  dispatch =>
-    fetch('http://localhost:5000/students')
-    .then(res =>res.json())
-    .then(data=> {
+import { BASE_URL } from '../../config';
+
+export const fetchStudentList = () => dispatch =>
+  fetch(`${BASE_URL}/mis/students`)
+    .then(res => res.json())
+    .then(data => {
       dispatch({
         type: 'FETCH_STUDENT_LIST',
-        studentList: data,
+        studentList: data.student
       });
     });
 
-export const addStudent = (student) =>
-  dispatch =>
-    fetch('http://localhost:5000/students', {
-      method: 'POST',
-      body: JSON.stringify(student),
-      headers: {
-        'Content-Type': 'Application/json'
-      }
-    })
-    .then(res =>res.json())
-    .then(data=> {
+export const addStudent = student => dispatch =>
+  fetch(`${BASE_URL}/mis/students`, {
+    method: 'POST',
+    body: JSON.stringify(student),
+    headers: {
+      'Content-Type': 'Application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
       dispatch({
         type: 'ADD_STUDENT',
-        student: data,
+        student: data.data
       });
     });
 
-export const updateStudent = (student) =>
-  dispatch =>
-    fetch(`http://localhost:5000/students/${student.id}`, {
-      method: 'PUT',
-      body: JSON.stringify(student),
-      headers: {
-        'Content-Type': 'Application/json'
-      }
-    })
-    .then(res =>res.json())
-    .then(data=> {
+export const updateStudent = student => dispatch =>
+  fetch(`${BASE_URL}/mis/students/${student.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(student),
+    headers: {
+      'Content-Type': 'Application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
       dispatch({
         type: 'UPDATE_STUDENT',
-        student: data,
+        student: data.data
       });
     });
 
-  export const removeStudent = (student) =>
-    dispatch =>
-      fetch(`http://localhost:5000/students/${student.id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'Application/json'
-        }
-      })
-      .then(res =>res.json())
-      .then(data=> {
-        dispatch({
-          type: 'REMOVE_STUDENT',
-          student,
-        });
+export const removeStudent = student => dispatch =>
+  fetch(`${BASE_URL}/mis/students/${student.id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'Application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      dispatch({
+        type: 'REMOVE_STUDENT',
+        student
       });
+    });
